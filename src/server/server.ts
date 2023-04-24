4573956a98a7c61ff57eeb2abc00ee8f51969f00
Molly Greene <mollycgreene@gmail.com>
2023-04-24 14:51:41 -0400

    Route skeleton & cookie parser

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,45 @@
-import express from 'express'; 
+import express from 'express';
 const app = express();
 app.use(express.json());
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
+
+// Root
+app.get('/', (req, res) => {
+    // If logged in, direct to homepage
+    // Else direct to login
+})
+
+// Login
+app.post('/login', (req, res) => {
+    // Authenticate user
+    // Set cookies
+    // Start session
+})
+
+// Signup
+app.get('/signup', (req, res) => {
+    // Serve signup page
+})
+
+app.post('/signup', (req, res) => {
+    // Create user
+    // Set cookies
+    // Start session
+})
+
+// 404: 
+app.use('*', (req, res) => {
+    res.status(404).send('Not Found');
+});
+
+// Global error handler: Not sure how to do this in Typescript!
+// @ts-expect-error 
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send({ error: err });
+});
