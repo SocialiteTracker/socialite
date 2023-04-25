317e5b4e638a0db7ffd71141f65cc4095967d759
andrewaltman1 <55675406+andrewaltman1@users.noreply.github.com>
2023-04-25 11:41:20 -0400

    setting up server tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -13,4 +13,40 @@ describe('Server routes', () => {
     const response = await request(app).get('/cannotfindit');
     expect(response.statusCode).toBe(404);
   });
+
+  test('It should respone with 200 to root', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('It should respone with 200 getting login', async () => {
+    const response = await request(app).get('/login');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('It should respone with 200 posting to login', async () => {
+    const response = await request(app).post('/login');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('It should respone with 200 getting signup', async () => {
+    const response = await request(app).get('/signup');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('It should respone with 200 posting to signup', async () => {
+    const response = await request(app).post('/signup');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('Global Error handler should catch error', async () => {
+    const response = await request(app).get('/', (req, res, next) => {
+      const error = new Error;
+      next(error)
+    });
+    expect(response.statusCode).toBe(500);
+  });
+
+  
 });
+
