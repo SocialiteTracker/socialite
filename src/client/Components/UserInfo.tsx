2ab770610ca554d0250aa4078caad1d2d1091296
gilessteiner1 <gilessteiner1@gmail.com>
2023-04-25 11:35:47 -0400

    added parameterized route for getting a users links

--- src/client/Components/UserInfo.tsx
+++ src/client/Components/UserInfo.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { useParams } from "react-router-dom";
 
 function UserInfo(){
 
     const { username } = useParams<{ username: string }>();
 
+    //Make database call here to get info about user
+    //Giles
+    function getUserLinks(){
+    }
+
+    //when the component mounts invoke getUserLinks
+    useEffect(()=>{
+        getUserLinks();
+    })
+
     return (
-        <h1> Getting links for: {username} </h1>
+        <h1 className="text-4xl"> Getting links for {username}... </h1>
     )
 }
 
