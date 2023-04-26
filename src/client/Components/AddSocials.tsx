979a604472347b79c95cd0596da4094bae212e3b
gilessteiner1 <gilessteiner1@gmail.com>
2023-04-26 14:39:05 -0400

    cleared input boxes on button press

--- src/client/Components/AddSocials.tsx
+++ src/client/Components/AddSocials.tsx
@@ -16,8 +16,8 @@ function AddSocials(props: addSocialsProps){
     return (
         <>
             <div className="flex justify-center">
-                <input placeholder="Social Media Name" className="inline-block mx-3 sm:pl-4 pl-3 py-4 mb-6 w-[45%] border border-gray-300 box-border rounded-lg" onChange={e => setNewSocialMedia(e.target.value)} required/>
-                <input placeholder="URL" className="inline-block mx-3 sm:pl-4 pl-3 px-2 py-4 mb-6 w-[45%] border border-gray-300 box-border rounded-lg" onChange={e => setNewUrl(e.target.value)} required/>
+                <input placeholder="Social Media Name" id="socialMediaInput" className="inline-block mx-3 sm:pl-4 pl-3 py-4 mb-6 w-[45%] border border-gray-300 box-border rounded-lg" onChange={e => setNewSocialMedia(e.target.value)} required/>
+                <input placeholder="URL" id="urlInput" className="inline-block mx-3 sm:pl-4 pl-3 px-2 py-4 mb-6 w-[45%] border border-gray-300 box-border rounded-lg" onChange={e => setNewUrl(e.target.value)} required/>
             </div>
             <div className="flex justify-center"> 
                 <button type="submit" className="bg-[#1f61a7] text-white text-xl mt-3 mb-7 py-5 hover:opacity-60 cursor-pointer sm:w-[15%] w-[30%] rounded-md" onClick={()=> {
@@ -27,11 +27,18 @@ function AddSocials(props: addSocialsProps){
                         const newSocialsState = props.socials.slice();
                         let duplicates = false;
                         newSocialsState.forEach((el)=>{
-                            if(el.socialMedia === newSocialMedia) el.url = newUrl;
-                            duplicates = true;
+                            if(el.socialMedia === newSocialMedia){
+                                el.url = newUrl;
+                                duplicates = true;
+                            }
                         });
                         if(duplicates){
                             props.addNewSocialMediaToState([...newSocialsState],newSocialMedia,newUrl);
+                            //@ts-ignore
+                            document.querySelector("#socialMediaInput").value = ""; 
+
+                            //@ts-ignore
+                            document.querySelector("#urlInput").value = "";
                             return;
                         }
 
@@ -40,6 +47,12 @@ function AddSocials(props: addSocialsProps){
                             socialMedia: newSocialMedia,
                             url: newUrl
                         }],newSocialMedia,newUrl);
+
+                        //@ts-ignore
+                        document.querySelector("#socialMediaInput").value = ""; 
+
+                        //@ts-ignore
+                        document.querySelector("#urlInput").value = "";
                     }
                     }
                 }>Add Social</button>
