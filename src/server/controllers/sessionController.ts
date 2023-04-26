6b633fe6bcf2131ffa8106e55c8c9d28c8cdd009
andrewaltman1 <55675406+andrewaltman1@users.noreply.github.com>
2023-04-26 14:05:15 -0400

    commit before revert

--- src/server/controllers/sessionController.ts
+++ src/server/controllers/sessionController.ts
@@ -20,39 +20,24 @@ import { Request, Response, NextFunction } from 'express';
 
 const SessionController = {
 
-    checkLogin: async (req: any, res: Response, next: NextFunction) => {
-
-        const userId = req.session.userId; //pull userId from request body
-
-        // implement later 
-        const findCookie = 'SELECT * FROM cookies WHERE user_id=$1';
-        const values = [userId];
-        const response = await pool.query(findCookie, values);
-        const cookie = response.rows;
-
-        if(cookie.length > 0) res.locals.authenticated = true; //current session 
+    checkLogin: (req: any, res: Response, next: NextFunction) => {
+        if(req.session.isAuthenticated) res.locals.authenticated = true; //current session 
         else res.locals.authenticated = false; //no current session 
-
         return next();
     },
 
     startSession: async (req: any, res: Response, next: NextFunction) => {
-
-        // if(res.locals.authenticated){
-        //     const addCookie = 'INSERT INTO cookies (created_at,user_id) VALUES ($1,$2)';
-        //     const values = [new Date().toTimeString().split(' ')[0],res.locals.userId];
-        //     const response = await pool.query(addCookie, values);   
         session({
-            store: new (require('connect-pg-simple')(session))({
+           store: new (require('connect-pg-simple')(session))({
            pool: pool,
-           tableName : 'user_sessions',
+           tableName: 'user_sessions',
            createTableIfMissing: true,
          }),
            secret: process.env.SESSION_SECRET,
            resave: false,
            saveUninitialized: true,
            cookie: { maxAge: 24 * 60 * 60 * 1000, secure: true}
-         })
+         });
          req.session.isAuthenticated = res.locals.authenticated;
          req.session.userId = res.locals.userId;
         return next();
