1c641b58bb1143a867cadb023bf45f1ce92b3bc1
andrewaltman1 <55675406+andrewaltman1@users.noreply.github.com>
2023-04-26 14:07:21 -0400

    Revert "added expression session"
    This reverts commit 743d794d47913671e6f147e9fb3b90750ee17f9e.

--- src/server/controllers/sessionController.ts
+++ src/server/controllers/sessionController.ts
@@ -1,45 +1,32 @@
 // require in session model
-const session = require('express-session');
 const pool = require('../config/connect')
-
-
-// app.use(session({
-//      store: new (require('connect-pg-simple')(session))({
-//     pool: pool,
-//     tableName : 'user_sessions',
-//     createTableIfMissing: true,
-
-//   }),
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { maxAge: 24 * 60 * 60 * 1000, secure: true}
-//   }))
 
 import { Request, Response, NextFunction } from 'express';
 
 const SessionController = {
 
-    checkLogin: (req: any, res: Response, next: NextFunction) => {
-        if(req.session.isAuthenticated) res.locals.authenticated = true; //current session 
+    checkLogin: async (req: Request, res: Response, next: NextFunction) => {
+
+        const userId = req.cookies.userId; //pull userId from request body
+
+        // implement later 
+        const findCookie = 'SELECT * FROM cookies WHERE user_id=$1';
+        const values = [userId];
+        const response = await pool.query(findCookie, values);
+        const cookie = response.rows;
+
+        if(cookie.length > 0) res.locals.authenticated = true; //current session 
         else res.locals.authenticated = false; //no current session 
         return next();
     },
 
-    startSession: async (req: any, res: Response, next: NextFunction) => {
-        session({
-           store: new (require('connect-pg-simple')(session))({
-           pool: pool,
-           tableName: 'user_sessions',
-           createTableIfMissing: true,
-         }),
-           secret: process.env.SESSION_SECRET,
-           resave: false,
-           saveUninitialized: true,
-           cookie: { maxAge: 24 * 60 * 60 * 1000, secure: true}
-         });
-         req.session.isAuthenticated = res.locals.authenticated;
-         req.session.userId = res.locals.userId;
+    startSession: async (req: Request, res: Response, next: NextFunction) => {
+
+        if(res.locals.authenticated){
+            const addCookie = 'INSERT INTO cookies (created_at,user_id) VALUES ($1,$2)';
+            const values = [new Date().toTimeString().split(' ')[0],res.locals.userId];
+            const response = await pool.query(addCookie, values);   
+        }
         return next();
     }
 };
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,3 @@
-import { Request } from "express"
-
-export interface SessionRequest extends Request {
-    session: Object
-  }
-
 export interface socialState {
     socialMedia: string,
     url: string
